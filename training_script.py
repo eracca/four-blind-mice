ee625781c2774a4cb043c774de4edfee88617a9e
eracca <eracca@udel.edu>
2017-11-09 23:25:55 +0000

    ready for training

--- training_script.py
+++ training_script.py
@@ -1,27 +1,39 @@
+#!/usr/bin/python
+
 import picamera as picam
 import RPi.GPIO as GPIO
 from time import sleep
 
-PATH = 'training_images/'
-TIME_IN_MINUTES = 1
-
-#TODO: fix led issue
+PATH = 'four-blind-mice/training_images/'
+TIME_IN_MINUTES = 0.5
 
 #setup
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(18, GPIO.OUT) 
+GPIO.output(18, GPIO.HIGH)
+sleep(3)
 GPIO.output(18, GPIO.LOW)
+
 pressed = False
-count = TIME_IN_MINUTES*60*2
+count = int(TIME_IN_MINUTES*60*2)
 camera = picam.PiCamera()
 
 #look for button press to start
 while not pressed:
-    pressed = GPIO.wait_for_edge(23, GPIO.RISING)
-print "Begin recording"
+    try:
+        pressed = GPIO.wait_for_edge(23, GPIO.RISING)
+    except KeyboardInterrupt:
+        break
 
-#record iamges
+print("Start recording")
+for i in range(3):
+    GPIO.output(18, GPIO.HIGH)
+    sleep(1)
+    GPIO.output(18, GPIO.LOW)
+    sleep(1)
+
+#record images
 while count >=0:
     camera.capture(PATH+ 'pic_'+str(count)+'.jpg')
     sleep(0.5)
@@ -29,5 +41,6 @@ while count >=0:
 
 print "Done recording"
 GPIO.output(18, GPIO.HIGH)
+sleep(5)
 
 GPIO.cleanup()
